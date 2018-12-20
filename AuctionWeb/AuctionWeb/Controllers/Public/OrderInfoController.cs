2f8f206f97268491a51d39facd6edf766953c9cd
651499253@qq.com <651499253@qq.com>
2018-12-20 10:10:48 +0800

    投诉功能实现

--- AuctionWeb/AuctionWeb/Controllers/Public/OrderInfoController.cs
+++ AuctionWeb/AuctionWeb/Controllers/Public/OrderInfoController.cs
@@ -21,7 +21,7 @@ namespace AuctionWeb.Controllers.AdminPage
             }
         }
 
-        public ActionResult Manage(string search)
+        public ActionResult Manage(string search, string EvaluateType)
         {
             //分页设置
             int pageIndex = Request.QueryString["pageIndex"] != null ? int.Parse(Request.QueryString["pageIndex"]) : 1;
@@ -32,26 +32,36 @@ namespace AuctionWeb.Controllers.AdminPage
             {
                 //个人用户
                 int purId = Convert.ToInt32(Session["PurId"]);
-                if (string.IsNullOrEmpty(search))
+                if (string.IsNullOrEmpty(search) && string.IsNullOrEmpty(EvaluateType))
                 {
                     mlist = DB.OrderInfo.Where(a => a.PurchaserId == purId).OrderByDescending(a => a.Id).Skip((pageIndex - 1) * pageSize).Take(pageSize).ToList<OrderInfo>();
                 }
                 else
                 {
-                    mlist = DB.OrderInfo.Where(a => a.PurchaserId == purId&&(a.Purchaser.Username.Contains(search) || a.Purchaser.TrueName.Contains(search) || a.GoodsInfo.GoodsName.Contains(search))).OrderByDescending(a => a.Id).Skip((pageIndex - 1) * pageSize).Take(pageSize).ToList<OrderInfo>();
+                    if (EvaluateType == "全部")
+                        mlist = DB.OrderInfo.Where(a => a.PurchaserId == purId && (a.Purchaser.Username.Contains(search) || a.Purchaser.TrueName.Contains(search) || a.GoodsInfo.GoodsName.Contains(search))).OrderByDescending(a => a.Id).Skip((pageIndex - 1) * pageSize).Take(pageSize).ToList<OrderInfo>();
+                    else if(EvaluateType == "投诉")
+                        mlist = DB.OrderInfo.Where(a => a.EvaluateState == "投诉" && a.PurchaserId == purId && (a.Purchaser.Username.Contains(search) || a.Purchaser.TrueName.Contains(search) || a.GoodsInfo.GoodsName.Contains(search))).OrderByDescending(a => a.Id).Skip((pageIndex - 1) * pageSize).Take(pageSize).ToList<OrderInfo>();
+                    else
+                        mlist = DB.OrderInfo.Where(a => a.EvaluateState != "投诉" && a.PurchaserId == purId && (a.Purchaser.Username.Contains(search) || a.Purchaser.TrueName.Contains(search) || a.GoodsInfo.GoodsName.Contains(search))).OrderByDescending(a => a.Id).Skip((pageIndex - 1) * pageSize).Take(pageSize).ToList<OrderInfo>();                 
                 }
                 listCount = DB.OrderInfo.Where(a => a.PurchaserId == purId).Count();
             }
             else
             {
                 //管理员
-                if (string.IsNullOrEmpty(search))
+                if (string.IsNullOrEmpty(search) && string.IsNullOrEmpty(EvaluateType))
                 {
                     mlist = DB.OrderInfo.Where(a => true).OrderByDescending(a => a.Id).Skip((pageIndex - 1) * pageSize).Take(pageSize).ToList<OrderInfo>();
                 }
                 else
                 {
-                    mlist = DB.OrderInfo.Where(a => a.Purchaser.Username.Contains(search) || a.Purchaser.TrueName.Contains(search) || a.GoodsInfo.GoodsName.Contains(search)).OrderByDescending(a => a.Id).Skip((pageIndex - 1) * pageSize).Take(pageSize).ToList<OrderInfo>();
+                    if (EvaluateType == "全部")
+                        mlist = DB.OrderInfo.Where(a => a.Purchaser.Username.Contains(search) || a.Purchaser.TrueName.Contains(search) || a.GoodsInfo.GoodsName.Contains(search)).OrderByDescending(a => a.Id).Skip((pageIndex - 1) * pageSize).Take(pageSize).ToList<OrderInfo>();
+                    else if (EvaluateType == "投诉")
+                        mlist = DB.OrderInfo.Where(a => a.EvaluateState == "投诉"  && (a.Purchaser.Username.Contains(search) || a.Purchaser.TrueName.Contains(search) || a.GoodsInfo.GoodsName.Contains(search))).OrderByDescending(a => a.Id).Skip((pageIndex - 1) * pageSize).Take(pageSize).ToList<OrderInfo>();
+                    else
+                        mlist = DB.OrderInfo.Where(a => a.EvaluateState != "投诉" && (a.Purchaser.Username.Contains(search) || a.Purchaser.TrueName.Contains(search) || a.GoodsInfo.GoodsName.Contains(search))).OrderByDescending(a => a.Id).Skip((pageIndex - 1) * pageSize).Take(pageSize).ToList<OrderInfo>();                               
                 }
                 listCount = DB.OrderInfo.Where(a => true).Count();
             }
@@ -96,12 +106,37 @@ namespace AuctionWeb.Controllers.AdminPage
         }
         [HttpPost]
         //投诉功能
-        public ActionResult Evaluate(int id, string Evaluate)
+        public ActionResult Evaluate(OrderInfo Info)
         {
-
-
+            Info.EvaluateState = "投诉";
+            Info.EvaluateTime = DateTime.Now;
+            DB.Entry(Info).State = System.Data.EntityState.Modified;
+            DB.SaveChanges();
+            return RedirectToAction("Manage");
+        }
+        //撤销投诉
+        public ActionResult CancelEvaluate(int id)
+        {
+            var Info = DB.OrderInfo.FirstOrDefault(a => a.Id == id);
+            Info.Evaluate = string.Empty;
+            Info.EvaluateState = string.Empty;
+            Info.EvaluateTime = null;
+            DB.Entry(Info).State = System.Data.EntityState.Modified;
+            DB.SaveChanges();
             return RedirectToAction("Manage");
         }
 
+        //处理投诉
+        public ActionResult DoneEvaluate(int id)
+        {
+            var Info = DB.OrderInfo.FirstOrDefault(a => a.Id == id);
+
+            Info.EvaluateState = "已处理";
+          
+            DB.Entry(Info).State = System.Data.EntityState.Modified;
+            DB.SaveChanges();
+            return RedirectToAction("Manage");
+        }
+        
     }
 }
